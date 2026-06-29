305b2fa77583a5038efc808dfbf5a1f049916195
vuyo-duke <vuyodukashe23@gmail.com>
2026-06-29 18:00:56 -0400

    fix: correct guess loop logic, comparisons, and database queries

--- number_guess.sh
+++ number_guess.sh
@@ -1,41 +1,53 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
-randoNum=$(( RANDOM % 1000 + 1 ))
+secretNumber=$(( RANDOM % 1000 + 1 ))
 echo -e "Enter your username:"
 read USERNAME
 
-echo-e "Guess the secret number between 1 and 1000:"
-read GUESS
 
-if [[ "$USERNAME" ]]
+
+USERNAME_EXISTS=$($PSQL "SELECT username FROM user_table WHERE username='$USERNAME'")
+if [[ -z "$USERNAME_EXISTS" ]]
 then 
+echo "Welcome, $USERNAME! It looks like this is your first time here."
+INSERT_USER=$($PSQL "INSERT INTO user_table(username) VALUES('$USERNAME')")
+else
+GAMES_PLAYED=$($PSQL "SELECT games_played FROM user_table WHERE username='$USERNAME'")
+BEST_GAME=$($PSQL "SELECT best_game FROM user_table WHERE username='$USERNAME'")
 echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
 fi
+nOfGuesses=0
+echo -e "Guess the secret number between 1 and 1000:"
+while true
+do
+read GUESS
 
-if [[ ! "$GUESS" =~ ^[ 0-9]+$ ]]
+if [[ ! "$GUESS" =~ ^[0-9]+$ ]]
 then
 echo "That is not an integer, guess again:"
+continue
 fi
 
-if [[ -z $USERNAME ]]
-then
-echo "Welcome, $USERNAME! It looks like this is your first time here."
-fi
+nOfGuesses=$(( nOfGuesses + 1 ))
 
-if [[ $GUESS === $randoNum ]]
+if [[ $GUESS -eq $secretNumber ]]
 then
 echo "You guessed it in $nOfGuesses tries. The secret number was $secretNumber. Nice job!"
+UPDATE_GAMES=$($PSQL "UPDATE user_table SET games_played = games_played + 1 WHERE username='$USERNAME'")
+if [[ -z $BEST_GAME ]] || [[ $nOfGuesses -lt $BEST_GAME ]]
+then
+UPDATE_BEST=$($PSQL "UPDATE user_table SET best_game = $nOfGuesses WHERE username='$USERNAME'")
 fi
+break
 
-if [[ $GUESS > $randoNum ]]
+elif [[ $GUESS -gt $secretNumber ]]
 then
 echo "It's higher than that, guess again:"
-fi
 
-if [[ $GUESS < $randoNum ]]
+elif [[ $GUESS -lt $secretNumber ]]
 then
 echo "It's lower than that, guess again:"
 fi
-
+done
 # games_played (count), best_game (min guesses), number of guesses (count), secret number (randoNum)
