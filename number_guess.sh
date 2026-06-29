10855d34d2ca062276b96ef0beac41ca7ea06629
vuyo-duke <vuyodukashe23@gmail.com>
2026-06-29 15:52:06 -0400

    chore: just finished the first draft of the script, next step will be to make all the tasks work

--- number_guess.sh
+++ number_guess.sh
@@ -5,6 +5,37 @@ randoNum=$(( RANDOM % 1000 + 1 ))
 echo -e "Enter your username:"
 read USERNAME
 
-if [[ -z username ]]
+echo-e "Guess the secret number between 1 and 1000:"
+read GUESS
+
+if [[ "$USERNAME" ]]
 then 
 echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+fi
+
+if [[ ! "$GUESS" =~ ^[ 0-9]+$ ]]
+then
+echo "That is not an integer, guess again:"
+fi
+
+if [[ -z $USERNAME ]]
+then
+echo "Welcome, $USERNAME! It looks like this is your first time here."
+fi
+
+if [[ $GUESS === $randoNum ]]
+then
+echo "You guessed it in $nOfGuesses tries. The secret number was $secretNumber. Nice job!"
+fi
+
+if [[ $GUESS > $randoNum ]]
+then
+echo "It's higher than that, guess again:"
+fi
+
+if [[ $GUESS < $randoNum ]]
+then
+echo "It's lower than that, guess again:"
+fi
+
+# games_played (count), best_game (min guesses), number of guesses (count), secret number (randoNum)
