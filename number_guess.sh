18723e6c8b52f3c4231e5cfbe017f1afa316d573
vuyo-duke <vuyodukashe23@gmail.com>
2026-06-29 06:37:38 -0400

    feat: created games_table and have started constructing sentences for the user to see

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,7 @@ PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 randoNum=$(( RANDOM % 1000 + 1 ))
 echo -e "Enter your username:"
 read USERNAME
+
+if [[ -z username ]]
+then 
+echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
