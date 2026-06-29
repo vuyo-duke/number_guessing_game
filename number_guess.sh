881d3079e6ed22400ab49e300609663f2fdd0ff0
vuyo-duke <vuyodukashe23@gmail.com>
2026-06-29 18:09:23 -0400

    chore: deleted a comment

--- number_guess.sh
+++ number_guess.sh
@@ -50,4 +50,4 @@ then
 echo "It's lower than that, guess again:"
 fi
 done
-# games_played (count), best_game (min guesses), number of guesses (count), secret number (randoNum)
+# games_played (count), best_game (min guesses), number of guesses (count)
